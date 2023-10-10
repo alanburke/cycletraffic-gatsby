314d054713cad2ff14d36ca34573e56010b69a95
Alan Burke <alan@cycletraffic.com>
2023-10-10 23:22:34 +0200

    Use Gatsby Remark images

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,24 @@ module.exports = {
     author: `@alanjosephburke`,
   },
   plugins: [
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 590,
+              quality: 50
+            },
+          },
+        ],
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -89,7 +107,6 @@ module.exports = {
       }
     },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-plugin-manifest`,
